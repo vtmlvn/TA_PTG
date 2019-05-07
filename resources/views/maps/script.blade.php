d83fd1caca22dfadc20a9572ab2efbca92474576
Vito Ompusunggu <theprivateeye27@gmail.com>
2019-05-08 06:56:19 +0700

    add single marker + circle

--- resources/views/maps/script.blade.php
+++ resources/views/maps/script.blade.php
@@ -1,5 +1,6 @@
 <script>
     function initMap() {
+        var circles=[], markerPin = [], globalLatlng;
         var options = {
             zoom : 14, 
             center:{lat:-6.59768, lng:106.799574}
@@ -9,12 +10,22 @@
 
         
         //Listen for click on map
-        google.maps.event.addListener(map, 'click', 
-        function(event){
-            //Marker
+        google.maps.event.addListener(map, 'click', function(event) {
+            console.log('f_in');
+            // marker.setMap(null);
+            deleteMarker();
             addMarker({coords:event.latLng});
+            
         });
 
+        function deleteMarker(){
+            for (var i=0; i<markerPin.length; i++){
+                markerPin[i].setMap(null);
+                circles[i].setMap(null);
+            }
+            markerPin=[];
+            circles=[]
+        }
         var S_marker=[];
         var i=0;
 
@@ -47,30 +58,64 @@
             
             });
 
-        // function schoolMarker(){
-        //     for (var i=0; i<myLatlng.lenght; i++){
-        //         marker[i] = new google.maps.Marker({
-        //         position: myLatlng[i],
-        //         map: map,
-        //         // title: myS[i]
-        //     });
-        //     }
-        // };
-        
         function addMarker(props){
             var marker = new google.maps.Marker({
                 position:props.coords,
                 map:map,
+                icon:props.icon,
             });
+
+            markerPin.push(marker);
             if(props.content){
                 var infoWindow = new google.maps.InfoWindow({
                 content:props.content.nama,
             });
             marker.addListener('click', function(){
                 infoWindow.open(map,marker);
-            });
-
+            // console.log('f_in');
+                error_log('This is some useful information.');
+                });
             }
+            circle = new google.maps.Circle({
+                map: map,
+                center: props.coords,
+                radius: 3000,
+                strokeColor: '#07A8BD',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#07A8BD',
+                fillOpacity: 0.1,
+                clickable: false
+            });
+            circles.push(circle);
+        }
+
+        function mapNul(){
+        var banyak = S_marker.length;
+        for (var i=0; i<banyak; i++){
+            S_marker[i].setMap(null);
+        }
+        circle.setMap(null);
+        markerPin.setMap(null);
+        S_marker = [];
+    }
+        function getDistanceFromLatLonInKm(lat2,lon2) {
+            var lat1 = globalLatlng.lat();
+            var lon1 = globalLatlng.lng();
+            var R = 6371; // Radius of the earth in km
+            var dLat = deg2rad(lat2-lat1);  // deg2rad below
+            var dLon = deg2rad(lon2-lon1); 
+            var a = 
+                Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+                Math.sin(dLon/2) * Math.sin(dLon/2)
+                ; 
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            var d = R * c; // Distance in km
+            return d;
+        }
+        function deg2rad(deg) {
+            return deg * (Math.PI/180)
         }
     }
 </script>
